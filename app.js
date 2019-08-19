3aa4d76ce33e37e61fd741de37ddfaac03e28aa0
Chihyun-Ahn <anchi15@naver.com>
2019-08-19 10:59:00 +0900

    20190819 10:58AM

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ exec("sudo ip link set can0 up type can bitrate 500000", function(err, stdout, s
 });
 
 var can = require('socketcan');
-var fs = require('fs');
+const math = require('math');
+
+// var fs = require('fs');
 var sensorLib = require('node-dht-sensor');
 
 // Parse database
@@ -52,6 +54,8 @@ var sensor = {
     }
 };
 
+
+
 setInterval(function(){
     sensor.read();
     var sensorData = {
@@ -59,29 +63,41 @@ setInterval(function(){
         temperature2 : 0,
         humidity1: "",
         humidity2: "",
+        sigTime: ""
     };
 
-    var controlData = {
-        fan1: 0,
-        fan2: 0,
-        fan3: 0,
-        feed: 0,
-        water: 0,
-        alarm: 0
-    };
+    // var controlData = {
+    //     fan1: 0,
+    //     fan2: 0,
+    //     fan3: 0,
+    //     feed: 0,
+    //     water: 0,
+    //     alarm: 0
+    // };
 
     sensorData.temperature1 = sensor.sensors[0].temperature;
     sensorData.temperature2 = sensor.sensors[1].temperature;
-    sensorData.humidity1 = sensor.sensors[0].humidity;
-    sensorData.humidity2 = sensor.sensors[1].humidity;
+    sensorData.humidity1    = sensor.sensors[0].humidity;
+    sensorData.humidity2    = sensor.sensors[1].humidity;
+    sensorData.sigTime      = getTimeInt();
+
+    console.log(sensorData.sigTime);
 
     db.messages["House1Stat"].signals["temperature1"].update(sensorData.temperature1);
     db.messages["House1Stat"].signals["temperature2"].update(sensorData.temperature2);
     db.messages["House1Stat"].signals["humidity1"].update(sensorData.humidity1);
     db.messages["House1Stat"].signals["humidity2"].update(sensorData.humidity2);
-
+    db.messages["House1Stat"].signals["sigTime"].update(sensorData.sigTime);
+    
+    console.log(db.messages["House1Stat"].signals["sigTime"].value);
+    
     // Trigger sending this message
     db.send("House1Stat");
 
+}, 6000);
 
-}, 3000);
+function getTimeInt(){
+    var now = new Date();
+    var nowInt = math.floor(now/1000);
+    return nowInt;
+}
--- test.js
+++ test.js
@@ -1,9 +1,19 @@
-var exec = require('child_process').exec;
+const math = require('math');
+var now = new Date();
+var timezone = 9; //Korea is 9 hours faster than Britain. 
+console.log(now);
+var nowInt = math.floor(now / 1000);
+console.log(nowInt);
+var nowIntKorea = nowInt + 9*60*60;
+console.log(nowIntKorea);
+console.log(new Date(nowIntKorea * 1000));
 
-exec("sudo ip link set can0 up type can bitrate 500000", function(err, stdout, stderr){
-    console.log('stdout: \n'+ stdout);
-    console.log('stderr: '+ stderr);
-    if(err != null){
-        console.log('error: '+ err);
-    }
-});
+// function aaaa(now, timezone){
+//     var britainTime = math.floor(now / 1000);
+//     var koreaTime   = britainTime + timezone*60*1000;
+//     var nowKorea = new Date(koreaTime*1000);
+//     console.log('Britain: ' +now);
+//     console.log('Korea: ' +nowKorea);
+// }
+
+// aaaa(now, timezone);
